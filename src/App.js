91774c4d2b18ac6e24c2fd2beb5760e05619276e
abalshakou <abalshakou@scnsoft.com>
2022-07-14 13:22:14 +0300

    Add countdown timer, add dependencies of it on localstorage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {useEffect, useState} from 'react';
 import './App.css';
 import {ethers} from "ethers";
 import EnglishAuction from "./artifacts/contracts/EnglishAuction.sol/EnglishAuction.json";
+import Countdown from 'react-countdown';
 
 
 const englishAuctionAddress = "0x98076b6f013d8377386373C193D5f56E26bC7DC4";
@@ -15,6 +16,9 @@ function App() {
     const [contract, setContract] = useState(null);
     const isConnected = Boolean(accounts[0]);
     const step = 5;
+    const timeDuration = 600000; //10 min
+
+    const Completionist = () => <span>Auction ended!</span>;
 
     const [bid, setBid] = useState(0);
     const [started, setStarted] = useState(false);
@@ -60,8 +64,17 @@ function App() {
         const transaction = await contract.start({
             gasLimit: 100000,
         });
-        await transaction.wait();
+        await transaction.wait()
+            .then(() => {
+
+                let startTimer = true;
+                localStorage.startTimer = startTimer;
+
+                let startTime = new Date();
+                localStorage.startTime = startTime;
+            });
         console.log('response: ', transaction);
+
     }
 
     const handleEnd = async () => {
@@ -80,13 +93,31 @@ function App() {
         });
         await transaction.wait();
         console.log('response: ', transaction);
-        await handleHighestBid();
+        await handlerHighestBid();
     }
 
     const handleIncrement = () => {
         console.log('  highestBid: ', highestBid);
         console.log('   Bid: ', bid);
         setBid(bid + step);
+    }
+
+    const checkTimer = () => {
+
+        if (localStorage.startTime) {
+            let a = Date.parse(localStorage.startTime);
+            let b = new Date();
+            console.log(b - a);
+
+            if ((b - a) > timeDuration) {
+                let startTimer = false;
+                localStorage.startTimer = startTimer;
+                localStorage.removeItem('startTime');
+            } else {
+                let startTimer = true;
+                localStorage.startTimer = startTimer;
+            }
+        }
     }
 
 
@@ -106,8 +137,19 @@ function App() {
             setContract(
                 contractEth
             );
+            Date.parse(localStorage.startTime)
 
             handlerHighestBid(contractEth);
+
+            const id = setInterval(() => {
+                handlerHighestBid(contractEth);
+                checkTimer();
+
+            }, 5000);
+
+            return () => {
+                clearInterval(id);
+            };
 
         } else {
             console.log("Please install Metamask!");
@@ -118,14 +160,6 @@ function App() {
 
         initConnection();
 
-        const id = setInterval(() => {
-            checkHighestBid();
-
-        }, 5000);
-
-        return () => {
-            clearInterval(id);
-        };
 
     }, []);
 
@@ -143,22 +177,35 @@ function App() {
                     <div className="py-3 p-7">Bid</div>
                     <div className="py-3 p-7">Team</div>
                     {isConnected &&
-                        <div className="bg-slate-900 text-white py-3 p-5 rounded">Connected</div>
+                    <div className="bg-slate-900 text-white py-3 p-5 rounded">Connected</div>
                     }
                 </div>
             </div>
             {!isConnected &&
-                <div className="mt-40">
-                    <button className=" bg-slate-700	 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                            onClick={connectAccount}>Connect
-                    </button>
-                </div>
+            <div className="mt-40">
+                <button className=" bg-slate-700	 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                        onClick={connectAccount}>Connect
+                </button>
+            </div>
             }
 
             {isConnected &&
             <>
-                <button className="py-3 p-5 underline" onClick={handleStart}>start</button>
-                <button className="py-3 p-5 underline" onClick={handleEnd}>end</button>
+                <div className="flex flex-row items-center justify-center">
+                    <button className="py-3 p-5 underline" onClick={handleStart}>start</button>
+                    <button className="py-3 p-5 underline" onClick={handleEnd}>end</button>
+                </div>
+
+                {localStorage.startTimer && JSON.parse(localStorage.startTimer) === true &&
+                    <Countdown date={Date.parse(localStorage.startTime) + timeDuration}>
+                        <Completionist/>
+                    </Countdown>
+                }
+
+                { localStorage.startTimer && JSON.parse(localStorage.startTimer) === false &&
+                    <Completionist/>
+                }
+
                 <div className="wrapper flex flex-col ">
                     <div className="bid-wrapper  ">
                         <figure className="elementor-image-box-img">
@@ -171,7 +218,9 @@ function App() {
 
 
                         <div>Your bid: {bid + step} wei</div>
-                        <button className=" bg-slate-100 hover:bg-slate-300 text-black font-bold py-1 px-2 rounded mr-5" onClick={handleIncrement}>+</button>
+                        <button className=" bg-slate-100 hover:bg-slate-300 text-black font-bold py-1 px-2 rounded mr-5"
+                                onClick={handleIncrement}>+
+                        </button>
                         <button className="underline" onClick={handleBid}>bid</button>
                     </div>
                 </div>
